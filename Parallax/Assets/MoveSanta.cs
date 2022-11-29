ca072402fa7e1ad7506762bc9ab0545261e5090c
Ben Smith <benSmith1981@users.noreply.github.com>
2022-11-29 17:04:35 +0000

    Added a score

--- Parallax/Assets/MoveSanta.cs
+++ Parallax/Assets/MoveSanta.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using System;
 public class MoveSanta : MonoBehaviour
 {
     Rigidbody2D rb;
+    public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI scoreText;
+    int highscore = 0;
+    int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        // if(PlayerPrefs.GetInt("HighScore") != Null){
+        //     highscore = PlayerPrefs.GetInt("HighScore");
+        // } else{
+        //     highscore = 0;
+        // }
+        highscore = PlayerPrefs.GetInt("HighScore");
+
+        highscoreText.text = highscoreText.text+": "+highscore;
+        scoreText.text = scoreText.text+": "+score;
+
     }
 
     // Update is called once per frame
@@ -22,5 +38,8 @@ public class MoveSanta : MonoBehaviour
             print("space key was pressed");
             rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
         }
+        score += (int)Math.Ceiling(Time.deltaTime);
+        scoreText.text = "Score: "+score;
+        PlayerPrefs.SetInt("Score",score);
     }
 }
